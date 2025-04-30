caf2c33370f7dcae0733f332a7bbbcab39f7cb60
Gil <gil@Gils-MacBook-Pro.local>
2025-04-30 18:14:02 +0300

    commit

--- src/server/s3/sdk.ts
+++ src/server/s3/sdk.ts
@@ -9,7 +9,6 @@ import {
   GetObjectCommandOutput,
 } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { appConfig } from '../../app.config';
 
 const AWS_BUCKET_NAME = "app-template-1252343"
 // Constants
